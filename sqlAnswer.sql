e31d596a8711b0cd949ccaf0abc7837240b61a8d
Muhammad Ariq Akbar <107034964+Illuminateee@users.noreply.github.com>
2024-02-29 14:25:16 +0700

    Update sqlAnswer.sql

--- sqlAnswer.sql
+++ sqlAnswer.sql
@@ -1,6 +1,6 @@
 -- misal memiliki table parent dan table siswa
 
-SELECT siswa.id, siswa.name, parent.name FROM siswa
+SELECT siswa.id, siswa.name, parent.name as parent_name FROM siswa
 LEFT JOIN parent ON siswa.id_parent = parent.id
 
 -- menggunakan left join agar null terbaca
